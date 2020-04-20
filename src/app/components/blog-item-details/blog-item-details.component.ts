a58d387c3b74e00d909c56d72dd644bccb390f75
Koyoo898 <wojtek78890@o2.pl>
2020-04-20 12:07:21 +0200

    lab7

--- src/app/components/blog-item-details/blog-item-details.component.ts
+++ src/app/components/blog-item-details/blog-item-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {DataService} from '../../services/data.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'blog-item-details',
@@ -8,12 +10,24 @@ import { Component, OnInit } from '@angular/core';
 export class BlogItemDetailsComponent implements OnInit {
 
 
-  image = 'http://osnews.pl/wp-content/uploads/2016/06/it-grafika.jpg';
-  text = 'Tytuł';
+  image = '';
+  text: string;
+  public id: number;
 
-  constructor() { }
+  constructor(private dataService: DataService, private  route: ActivatedRoute) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    let id: string;
+    this.route.paramMap
+    .subscribe(params => {
+      id = params.get('id');
+   });
+    this.dataService.getById(id).subscribe(res => {
+    this.image = res['image'];
+    this.text = res['text'];
+    });
+
   }
+
 
 }
