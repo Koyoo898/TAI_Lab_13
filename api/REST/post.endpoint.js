1803b0229cb9f75147bfd15a951ab70b553648c2
Koyoo898 <wojtek78890@o2.pl>
2020-05-11 14:10:42 +0200

    lab9

---
+++ api/REST/post.endpoint.js
@@ -0,0 +1,14 @@
+'use strict';
+import business from '../business/business.container';
+import applicationException from "../service/applicationException";
+const postEndpoint = (router) => {
+  router.get('/api/posts', async (request, response, next) => {
+    try {
+      let result = await business(request).getCategoryManager().query();
+      response.status(200).send(result);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+};
+export default postEndpoint;
