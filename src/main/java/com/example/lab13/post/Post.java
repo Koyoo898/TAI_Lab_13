f67edc647ae2394faec086305483f68f7a24d5ae
Koyoo898 <wojtek78890@o2.pl>
2020-06-16 20:22:30 +0200

    add spring-boot service

---
+++ src/main/java/com/example/lab13/post/Post.java
@@ -0,0 +1,57 @@
+package com.example.lab13.post;
+
+public class Post {
+    private long id;
+    private String title;
+    private String url;
+    private String comment;
+
+    public Post(long id, String title, String url, String comment) {
+        this.id = id;
+        this.title = title;
+        this.url = url;
+        this.comment = comment;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", url='" + url + '\'' +
+                ", comment='" + comment + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/lab13/post/PostController.java
@@ -0,0 +1,34 @@
+package com.example.lab13.post;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLOutput;
+import java.util.List;
+
+@RestController
+public class PostController {
+
+    private PostService postService;
+
+    @Autowired
+    public PostController(PostService postService){
+        this.postService =  postService;
+    }
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("/api/posts")
+    public List<Post> getAllPosts(){
+        return postService.getAll();
+    }
+
+    @CrossOrigin(origins = "http://localhost:4200")
+    @GetMapping("/api/posts/{id}")
+    public Post get(@PathVariable int id){
+        System.out.println(postService.get(id));
+        return postService.get(id);
+    }
+
+}
